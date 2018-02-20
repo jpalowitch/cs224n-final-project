420c09a9820eef800bd0751b22e28d0ea412ac22
johnpalowitch <johnpalowitch@gmail.com>
2018-02-19 21:40:49 -0800

    added util to convert 64-class labels to 64-class one-hots

--- project_utils.py
+++ project_utils.py
@@ -1,7 +1,4 @@
 import numpy as np
-import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
 
 kColumnNames = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', \
                 'identity_hate']
@@ -17,3 +14,18 @@ def get_base2_labels(rows):
   """
   base2_vec = [2 ** x for x in range(rows.shape[1])]
   return np.matmul(rows, base2_vec)
+
+def get_base2_onehots(rows):
+  """Converts a matrix of binary row vectors to one-hot label vectors.
+  
+  Args:
+    rows: an array of binary vectors.
+  Returns:
+    labels: a row matrix of one-hots giving base2 classes of rows.
+  """
+  base2_mat = np.zeros((rows.shape[0], pow(2, rows.shape[1])))
+  base2_list = list(get_base2_labels(rows))
+  one_vec = np.ones((rows.shape[0],))
+  base2_mat[range(rows.shape[0]), get_base2_labels(rows)] = one_vec
+  return base2_mat
+      
