58c4fb026abce6f183e53b412736379935d69636
jpalowitch <johnpalowitch@gmail.com>
2018-02-26 00:27:34 -0800

    made logistic baseline sparse; also added CLASSIFICATION category and cleaned up some indentation

--- logistic_baseline_tensorflow.py
+++ logistic_baseline_tensorflow.py
@@ -14,8 +14,9 @@ from sklearn.metrics import roc_auc_score
 
 import pandas as pd
 
-APPROACH = "Logistic"
-FLAVOR = "tf-Adam"
+APPROACH = "ngram"
+CLASSIFIER = "logistic"
+FLAVOR = "tensorflow-ADAM"
 
 # Parameters
 learning_rate = 0.01
@@ -25,22 +26,24 @@ display_step = 1
 
 # Get data and featurizing
 train, dev, test = get_TDT_split(pd.read_csv('train.csv').fillna(' '))
-train_vecs, dev_vecs, test_vecs = vectorize_corpus_tf_idf(train, dev, test)
+#train_vecs, dev_vecs, test_vecs = vectorize_corpus_tf_idf(train, dev, test)
+train_vecs, dev_vecs, test_vecs = vectorize_corpus_tf_idf(train, dev, test, sparse=True)
 n_train = train_vecs.shape[0]
 
 # tf Graph Input
-x = tf.placeholder(tf.float32, [None, NUM_FEATURES])
+#x = tf.placeholder(tf.float32, [None, NUM_FEATURES])
+x = tf.sparse_placeholder(tf.float32)
 y = tf.placeholder(tf.float32, [None, 2])
 
 # Set model weights
-W = tf.get_variable(
-      "weights",
-      shape=[NUM_FEATURES, 2],
-      initializer=tf.contrib.layers.xavier_initializer())
+W = tf.get_variable("weights",
+                    shape=[NUM_FEATURES, 2],
+                    initializer=tf.contrib.layers.xavier_initializer())
 b = tf.Variable(tf.zeros([2]))
 
 # Construct model
-pred = tf.nn.softmax(tf.matmul(x, W) + b) # Softmax
+#pred = tf.nn.softmax(tf.matmul(x, W) + b) # Softmax
+pred = tf.nn.softmax(tf.sparse_tensor_dense_matmul(x, W) + b) # Softmax
 
 # Minimize error using cross entropy
 cost = tf.reduce_mean(-tf.reduce_sum(y*tf.log(pred), reduction_indices=1))
@@ -50,48 +53,51 @@ optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
 # Final scoring
 def AUC_calc(X, Y): 
-  return roc_auc_score(Y[:, 1], pred.eval({x: X})[:, 1])
+    return roc_auc_score(Y[:, 1], pred.eval({x: X})[:, 1])
 
 # Initialize the variables (i.e. assign their default value)
 global_init = tf.global_variables_initializer()
 
 auc_scores = []
-for target_class in range(6):
+for target_class in range(1):
+    
+    # Getting labels for training
+    train_labels = train[CLASS_NAMES[target_class]].values
+    train_target = get_onehots_from_labels(train_labels)
+    test_labels = test[CLASS_NAMES[target_class]].values
+    test_target = get_onehots_from_labels(test_labels)
+    
+    # Start training
+    with tf.Session() as sess:
+    
+        # Run initializer
+        sess.run(global_init)
+        
+        # Training 
+        for epoch in range(training_epochs):
+            avg_cost = 0.
+            total_batch = int(n_train/batch_size)
+            
+            # Loop over batches
+            for i in range(total_batch):
+                upper_indx = min((i + 1) * batch_size, n_train)
+                i_indxs = range(i * batch_size, upper_indx)
+                batch_xs = sparse_tens_from_sparse_mat(train_vecs[i_indxs])
+                batch_ys = train_target[i_indxs]
+                _, c = sess.run([optimizer, cost], feed_dict={x: batch_xs,
+                                                              y: batch_ys})
+                avg_cost += c / total_batch
+            
+            # Display logs
+            if (epoch+1) % display_step == 0:
+              print("Epoch:", '%04d' % (epoch+1), 
+                    "cost=", "{:.9f}".format(avg_cost))
+        
+        print("Optimization Finished!")
+        AUC = AUC_calc(sparse_tens_from_sparse_mat(test_vecs), test_target)
+        print ("AUC_ROC:", AUC)
+        auc_scores.append(AUC)
+        
+        sess.close()
 
-  # Getting labels for training
-  train_labels = train[CLASS_NAMES[target_class]].values
-  train_target = get_onehots_from_labels(train_labels)
-  test_labels = test[CLASS_NAMES[target_class]].values
-  test_target = get_onehots_from_labels(test_labels)
-
-  # Start training
-  with tf.Session() as sess:
-    
-    # Run initializer
-    sess.run(global_init)
-    
-    # Training 
-    for epoch in range(training_epochs):
-      avg_cost = 0.
-      total_batch = int(n_train/batch_size)
-      # Loop over batches
-      for i in range(total_batch):
-        upper_indx = min((i + 1) * batch_size, n_train)
-        i_indxs = range(i * batch_size, upper_indx)
-        batch_xs = train_vecs[i_indxs]
-        batch_ys = train_target[i_indxs]
-        _, c = sess.run([optimizer, cost], feed_dict={x: batch_xs,
-                                                      y: batch_ys})
-        avg_cost += c / total_batch
-      # Display logs
-      if (epoch+1) % display_step == 0:
-        print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(avg_cost))
-    
-    print("Optimization Finished!")
-    AUC = AUC_calc(test_vecs, test_target)
-    print ("AUC_ROC:", AUC)
-    auc_scores.append(AUC)
-    
-    sess.close()
-
-save_auc_scores(auc_scores, APPROACH, FLAVOR)
+save_auc_scores(auc_scores, APPROACH, CLASSIFIER, FLAVOR)
--- project_utils.py
+++ project_utils.py
@@ -18,106 +18,113 @@ TRAIN_DATA_FILE = "train.csv"
 NUM_FEATURES = 10000
 
 def get_base2_labels(rows):
-  """Converts a matrix of binary row vectors to labels.
+    """Converts a matrix of binary row vectors to labels.
 
-  Args:
-    rows: an array of binary vectors.
-  Returns:
-    labels: a column vector of integers = base 2 versions of rows.
-  """
-  base2_vec = [2 ** x for x in range(rows.shape[1])]
-  return np.matmul(rows, base2_vec)
+    Args:
+      rows: an array of binary vectors.
+    Returns:
+      labels: a column vector of integers = base 2 versions of rows.
+    """
+    base2_vec = [2 ** x for x in range(rows.shape[1])]
+    return np.matmul(rows, base2_vec)
 
 def get_base2_onehots(rows):
-  """Converts a matrix of binary row vectors to one-hot label vectors.
+    """Converts a matrix of binary row vectors to one-hot label vectors.
 
-  Args:
-    rows: an array of binary vectors.
-  Returns:
-    labels: a row matrix of one-hots giving base2 classes of rows.
-  """
-  base2_mat = np.zeros((rows.shape[0], pow(2, rows.shape[1])))
-  base2_list = list(get_base2_labels(rows))
-  one_vec = np.ones((rows.shape[0],))
-  base2_mat[range(rows.shape[0]), get_base2_labels(rows)] = one_vec
-  return base2_mat
+    Args:
+      rows: an array of binary vectors.
+    Returns:
+      labels: a row matrix of one-hots giving base2 classes of rows.
+    """
+    base2_mat = np.zeros((rows.shape[0], pow(2, rows.shape[1])))
+    base2_list = list(get_base2_labels(rows))
+    one_vec = np.ones((rows.shape[0],))
+    base2_mat[range(rows.shape[0]), get_base2_labels(rows)] = one_vec
+    return base2_mat
 
 def get_onehots_from_labels(labels):
-  """Converts an np integer vector of labels into a matrix of one-hot vectors.
+    """Converts an np integer vector of labels into a matrix of one-hot vectors.
 
-  Args:
-    labels: an integer vector of labels:
-  Returns:
-    onehots: a row matrix of one-hots. Each row as a 1 in position i if i is the
-    position of the row's integer in the ordered integer labels.
-  """
-  unique_labs = list(set(labels))
-  label_indx = [unique_labs.index(x) for x in labels]
-  one_vec = np.ones((labels.shape[0], ))
-  onehots = np.zeros((labels.shape[0], len(unique_labs)))
-  onehots[range(labels.shape[0]), label_indx] = one_vec
-  return onehots
+    Args:
+      labels: an integer vector of labels:
+    Returns:
+      onehots: a row matrix of one-hots. Each row as a 1 in position i if i is the
+      position of the row's integer in the ordered integer labels.
+    """
+    unique_labs = list(set(labels))
+    label_indx = [unique_labs.index(x) for x in labels]
+    one_vec = np.ones((labels.shape[0], ))
+    onehots = np.zeros((labels.shape[0], len(unique_labs)))
+    onehots[range(labels.shape[0]), label_indx] = one_vec
+    return onehots
 
 def get_TDT_split(df, split_prop=SPLIT_PROP, seed=SPLIT_SEED):
-  """Takes pd.DataFrame from load of data and gives a train/dev split.
+    """Takes pd.DataFrame from load of data and gives a train/dev split.
 
-  Args:
-    data: a pd.DataFrame of the jigsaw data.
-    split_prop: a list of floats which is proportional to data split.
-    seed: an integer random seed for the split.
-  Returns:
-    train: training data.
-    dev: development data.
-    test: testing data.
-  """
-  ndata = [int(df.shape[0] * x / sum(split_prop)) for x in split_prop]
-  ndata[2] = ndata[2] + df.shape[0] - sum(ndata)
-  np.random.seed(seed)
-  df = df.sample(frac=1)
-  train = df[:ndata[0]]
-  dev = df[ndata[0]:(ndata[0] + ndata[1])]
-  test = df[-ndata[2]:]
-  return train, dev, test
+    Args:
+      data: a pd.DataFrame of the jigsaw data.
+      split_prop: a list of floats which is proportional to data split.
+      seed: an integer random seed for the split.
+    Returns:
+      train: training data.
+      dev: development data.
+      test: testing data.
+    """
+    ndata = [int(df.shape[0] * x / sum(split_prop)) for x in split_prop]
+    ndata[2] = ndata[2] + df.shape[0] - sum(ndata)
+    np.random.seed(seed)
+    df = df.sample(frac=1)
+    train = df[:ndata[0]]
+    dev = df[ndata[0]:(ndata[0] + ndata[1])]
+    test = df[-ndata[2]:]
+    return train, dev, test
 
-def sparse_mat_to_sparse_tensor(scipy_sparse):
-  """Converts a sparse matrix to a tensor object.
+def sparse_tens_from_sparse_mat(scipy_sparse):
+    """Produces needed input to tf.sparse_placeholder from a csr matrix
 
-  Args:
-    scipy_sparse: a scipy sparse CSR matrix
-  Returns:
-    tf_sparse: a tensorflow sparse matrix
-  """
-  coo = scipy_sparse.tocoo()
-  indices = np.mat([coo.row, coo.col]).transpose()
-  tf_sparse = tf.SparseTensor(indices, coo.data, coo.shape)
-  return tf_sparse
+    Args:
+      scipy_sparse: a scipy sparse CSR matrix
+    Returns:
+      sparse_input: a tuple to pass to sparse_placeholder, see examples at:
+        https://www.tensorflow.org/versions/r0.12/api_docs/python/io_ops/placeholders
+    """
+    coo = scipy_sparse.tocoo()
+    indices = np.mat([coo.row, coo.col]).transpose()
+    sparse_input = (indices, coo.data, coo.shape)
+    return sparse_input
 
-def save_auc_scores(scores, approach, flavor, 
+def save_auc_scores(scores, approach, classifier, flavor, 
                     fn="auc_scores.csv", overwrite=True):
-  """Records auc scores of approach-flavor run.
-  
-  Args:
-    scores: a list or array of 6 auc scores
-    approach: string that names the approach
-    flavor: string that names the flavor
-    fn: output filename
-    overwrite: if True, will overwrite a previous result with the same 
-      approach & flavor
-  Returns:
-    None
-  """
-  new_data_d = {"Approach": approach, "Flavor": flavor}
-  new_data_d.update(zip(CLASS_NAMES, scores))
-  if os.path.isfile(fn):
-    old_data = pd.read_csv(fn, index_col=0)
-    new_data = pd.DataFrame(data=new_data_d, index=[old_data.shape[0]])
-    old_data = old_data.append(new_data)
-    if overwrite:
-      old_data = old_data.drop_duplicates(subset=['Approach', 'Flavor'])
-  else:
-    old_data = pd.DataFrame(data=new_data_d, index=[0])
-  old_data.to_csv(fn)
-  return None
+    """Records auc scores of approach-flavor run.
+
+	   ***Before setting your approach/classifier/flavor strings, make sure to 
+   	      check out the existing auc_scores.csv for formatting. This will help
+		  later to visualize results from particular approaches/classifiers.***
+    
+    Args:
+      scores: a list or array of 6 auc scores
+      approach: string that names the approach
+      flavor: string that names the flavor
+      fn: output filename
+      overwrite: if True, will overwrite a previous result with the same 
+        approach & flavor
+    Returns:
+      None
+    """
+    new_data_d = {"Approach": approach, 
+                  "Classifier": classifier, 
+                  "Flavor": flavor}
+    new_data_d.update(zip(CLASS_NAMES, scores))
+    if os.path.isfile(fn):
+        old_data = pd.read_csv(fn, index_col=0)
+        new_data = pd.DataFrame(data=new_data_d, index=[old_data.shape[0]])
+        old_data = old_data.append(new_data)
+        if overwrite:
+            old_data = old_data.drop_duplicates(subset=['Approach', 'Flavor'])
+    else:
+        old_data = pd.DataFrame(data=new_data_d, index=[0])
+    old_data.to_csv(fn)
+    return None
     
 def vectorize_corpus_tf_idf(train, dev, test, path=TFIDF_VECTORS_FILE,
                             n_features=NUM_FEATURES, sparse=False):
