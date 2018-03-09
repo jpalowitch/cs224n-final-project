c780a0623ce74112d8af030f1943b3169c2ff003
Manav <manavk123@gmail.com>
2018-03-09 15:58:35 -0800

    fixes for running GloVe with actual corpus

--- glove.py
+++ glove.py
@@ -6,13 +6,13 @@ import numpy as np
 from collections import defaultdict
 import tensorflow as tf
 import os
-from project_utils import get_TDT_split
+from project_utils import get_TDT_split, get_development_data
 import pandas as pd
 import pickle
 
 
-def build_coccurrence_matrix(corpus, window_size=10, min_frequency=1):
-    """ Builds a cooccurence matrix as a dictionary.
+def build_coccurrence_matrix(corpus, window_size=10, min_frequency=0):
+    """ Builds a cooccurrence matrix as a dictionary.
 
     Args:
         corpus: list of sentences
@@ -20,10 +20,10 @@ def build_coccurrence_matrix(corpus, window_size=10, min_frequency=1):
         min_frequency: minimum frequency of a word to keep
 
     Returns:
-        cooccurence_matrix: dictionary of form {(center_word_index, context_word_index):count}
+        cooccurrence_matrix: dictionary of form {(center_word_index, context_word_index):count}
         tokenizer: word tokenizer to fetch information for word frequency and other values
     """
-    print 'Building cooccurence matrix'
+    print 'Building cooccurrence matrix'
     # train tokenizer on corpus
     tokenizer = Tokenizer()
     tokenizer.fit_on_texts(corpus)
@@ -32,7 +32,7 @@ def build_coccurrence_matrix(corpus, window_size=10, min_frequency=1):
     # dict of {token_index: word}
     word_index_reverse = {v:k for k, v in tokenizer.word_index.items()}
 
-    cooccurence_matrix_unfiltered = defaultdict(float)
+    cooccurrence_matrix_unfiltered = defaultdict(float)
     sequences = tokenizer.texts_to_sequences(corpus)
 
     for idx, token_ids in enumerate(sequences):
@@ -50,36 +50,36 @@ def build_coccurrence_matrix(corpus, window_size=10, min_frequency=1):
             # left context word
             for u_idx, u in enumerate(left_context_words):
                 distance = left_context_size - u_idx
-                cooccurence_matrix_unfiltered[(v, u)] += (1.0 / float(distance))
+                cooccurrence_matrix_unfiltered[(v, u)] += (1.0 / float(distance))
 
             # right context words
             for u_idx, u in enumerate(right_context_words):
                 distance = u_idx + 1
-                cooccurence_matrix_unfiltered[(v, u)] += (1.0 / float(distance))
+                cooccurrence_matrix_unfiltered[(v, u)] += (1.0 / float(distance))
 
     if min_frequency > 1:
         # matrix of (v, u) : count
-        cooccurence_matrix = defaultdict(float)
-        for (v, u), count in cooccurence_matrix_unfiltered.items():
+        cooccurrence_matrix = defaultdict(float)
+        for (v, u), count in cooccurrence_matrix_unfiltered.items():
             v_actual = word_index_reverse[v]
             u_actual = word_index_reverse[u]
             # print 'v: {} u: {} count: {}'.format(v, u, count)
             if tokenizer.word_counts[v_actual] >= min_frequency and tokenizer.word_counts[u_actual] >= min_frequency:
-                 cooccurence_matrix[(v, u)] = count
+                 cooccurrence_matrix[(v, u)] = count
 
         print "Completed building cooccurrence matrix"
-        return cooccurence_matrix, tokenizer
+        return cooccurrence_matrix, tokenizer
     else:
         print "Completed building cooccurrence matrix"
-        return cooccurence_matrix_unfiltered, tokenizer
+        return cooccurrence_matrix_unfiltered, tokenizer
 
 
-def build_graph_and_train(cooccurence_matrix, vocab_size):
+def build_graph_and_train(cooccurrence_matrix, vocab_size):
     """ Builds and trains a tensorflow model for creating GloVe vectors based off
-        of a cooccurence_matrix.
+        of a cooccurrence_matrix.
 
     Args:
-        cooccurence_matrix: dictionary of form {(input_word_index, context_word_index) : count}
+        cooccurrence_matrix: dictionary of form {(input_word_index, context_word_index) : count}
         vocab_size: size of vocabulary
 
     Returns:
@@ -97,23 +97,24 @@ def build_graph_and_train(cooccurence_matrix, vocab_size):
 
     # variables
     # weights for input (i) and context (j) vectors
-    W_i = tf.get_variable("W_i", shape=(vocab_size, embedding_size), initializer=tf.contrib.layers.xavier_initializer())
-    W_j = tf.get_variable("W_j", shape=(vocab_size, embedding_size), initializer=tf.contrib.layers.xavier_initializer())
+    # tokens are indexed at 1, so need shape needs to be vocab_size + 1
+    W_i = tf.get_variable("W_i", shape=[vocab_size + 1, embedding_size], initializer=tf.contrib.layers.xavier_initializer())
+    W_j = tf.get_variable("W_j", shape=[vocab_size + 1, embedding_size], initializer=tf.contrib.layers.xavier_initializer())
 
     # each word has a scalar weight for when it is a center or context word
-    B_i = tf.get_variable("input_bias", shape=(vocab_size), initializer=tf.constant_initializer)
-    B_j = tf.get_variable("context_bias", shape=(vocab_size), initializer=tf.constant_initializer)
+    B_i = tf.get_variable("input_bias", shape=[vocab_size + 1], initializer=tf.constant_initializer)
+    B_j = tf.get_variable("context_bias", shape=[vocab_size + 1], initializer=tf.constant_initializer)
 
-    # add placeholders for indices for the input and output words and cooccurence for (v, u)
+    # add placeholders for indices for the input and output words and cooccurrence for (v, u)
     i = tf.placeholder(tf.int32, shape=(batch_size), name="input_batch")
     j = tf.placeholder(tf.int32, shape=(batch_size), name="output_batch")
     X_ij = tf.placeholder(tf.float32, shape=(batch_size), name="context_batch")
 
     # weights and biases
-    w_i = tf.nn.embedding_lookup([W_i], i)
-    w_j = tf.nn.embedding_lookup([W_j], j)
-    b_i = tf.nn.embedding_lookup([B_i], i)
-    b_j = tf.nn.embedding_lookup([B_j], j)
+    w_i = tf.nn.embedding_lookup(W_i, i)
+    w_j = tf.nn.embedding_lookup(W_j, j)
+    b_i = tf.nn.embedding_lookup(B_i, i)
+    b_j = tf.nn.embedding_lookup(B_j, j)
 
     # actual math
     # f (X_ij): map the function count < max_count ? (count/max_count)^2 : 1 onto each element
@@ -137,14 +138,17 @@ def build_graph_and_train(cooccurence_matrix, vocab_size):
     # treating final word vectors as i + j
     combined_embeddings = W_i + W_j
 
-    # train!!
+    # train!
     with tf.Session() as sess:
         tf.global_variables_initializer().run()
         for epoch in range(num_epochs):
             # TODO: add minibatching, remove array syntax
             if epoch % 10 == 0:
                 print 'On epoch: {}'.format(epoch)
-            for (input_batch,context_batch), count_batch in cooccurence_matrix.items():
+            idx = 0
+            for (input_batch, context_batch), count_batch in cooccurrence_matrix.items():
+                if idx % 10000 == 0:
+                    print 'On iteration: {}'.format(idx)
                 # make sure everything is the right shape
                 # f_array = sess.run(f, feed_dict={X_ij: [200, 20]})
                 # x_array = sess.run(X_ij, feed_dict={X_ij: [200, 20]})
@@ -159,12 +163,13 @@ def build_graph_and_train(cooccurence_matrix, vocab_size):
                     X_ij: [count_batch]
                 }
                 sess.run(optimizer, feed_dict=feed_dict)
+                idx += 1
 
         embeddings = sess.run(combined_embeddings)
         return embeddings
 
-def get_cooccurence_matrices(path="data/cooccurence.pkl", load_files=True):
-    """ Builds and retuns the cooccurence matrices for the train, dev, and test sets
+def get_cooccurrence_matrices(path="data/cooccurrence.pkl", load_files=True):
+    """ Builds and retuns the cooccurrence matrices for the train, dev, and test sets
 
     Args:
         path: path for the pkl file
@@ -172,7 +177,7 @@ def get_cooccurence_matrices(path="data/cooccurence.pkl", load_files=True):
 
     Returns:
         matrices: dict with three keys (train, dev, test). Each is a dict that
-                  holds the corresponding cooccurence matrix and tokenizer.
+                  holds the corresponding cooccurrence matrix and tokenizer.
     """
     if os.path.isfile(path) and load_files:
         with open(path, "rb") as fp:
@@ -208,14 +213,36 @@ def get_cooccurence_matrices(path="data/cooccurence.pkl", load_files=True):
             pickle.dump(matrices, fp)
         return matrices
 
+def get_embeddings(cooccurrence_matrix, vocab_size, data_set, path="embeddings.pkl", load_files=True):
+    full_path = "data/" + data_set + "_" + path
+    if os.path.isfile(path) and load_files:
+        with open(full_path, "rb") as fp:
+            embeddings = pickle.load(fp)
+        return embeddings
+    else:
+        embeddings = build_graph_and_train(cooccurrence_matrix, vocab_size)
+        with open(full_path, "wb") as fp:
+            pickle.dump(embeddings, fp)
+        return embeddings
+
 ###### UTILS
 def get_sentence_from_tokens(tokenized_sentence, word_index_reverse):
+    """ Reconstructs sentence from list of tokens.
+
+    Args:
+        tokenized_sentence: list of tokens, where each token < vocab_size
+        word_index_reverse: a dictionary of shape {word_index: word}
+    Returns:
+        sentence: the reconstructed sentence
+    """
     sentence = [word_index_reverse[word] for word in tokenized_sentence]
     print 'tokens:    {}'.format(tokenized_sentence)
     print 'sentence:  {}'.format(sentence)
     return sentence
 
 def print_tokenizer_information(tokenizer, corpus):
+    """ Prints useful information about the tokenizer.
+    """
     print 'word_index'
     print tokenizer.word_index
     print 'word_counts (frequency)'
@@ -228,12 +255,16 @@ def print_tokenizer_information(tokenizer, corpus):
     print len(tokenizer.word_index.keys())
 
 def test_build_coccurrence_matrix():
+    """ Tests the model with an arbitrary data set
+    """
     corpus = ['San Francisco is in California.', 'California is a great place.', 'California is a subpar place.']
     cooccur = build_coccurrence_matrix(corpus, min_frequency=2)
     print cooccur
 
 def test_train():
-    # Build cooccurence matrix
+    """ Tests the cooccurrence matrix with a small dataset
+    """
+    # Build cooccurrence matrix
     corpus = ['San Francisco is in California.', 'California is a great place.', 'California is a subpar place.']
     cooccurrence_matrix, tokenizer  = build_coccurrence_matrix(corpus, min_frequency=2)
     vocab_size = len(tokenizer.word_index.keys())
@@ -241,7 +272,16 @@ def test_train():
     print 'final embeddings:'
     print embeddings
 
+def test_glove_model():
+    """Tests the model using the first fifteen elements in the training data sets
+    """
+    corpus = get_development_data()
+    cooccurrence_matrix, tokenizer  = build_coccurrence_matrix(corpus)
+    vocab_size = len(tokenizer.word_index.keys())
+    embeddings = build_graph_and_train(cooccurrence_matrix, vocab_size)
+    print 'final embeddings'
+    print embeddings
+
 if __name__ == '__main__':
-    matrices = get_cooccurence_matrices()
-    print 'first!'
-    print matrices.get("train").get("matrix").itervalues().next()
+    # test_train()
+    
